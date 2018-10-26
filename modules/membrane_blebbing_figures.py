2591eabc9ef79912fb67c7b207fb32d8defe47ae
douglas kelly <douglas.kelly@riken.jp>
2018-10-26 19:43:39 +0900

    WIP merge intensity - currently works-ish, but gives unexpected RBG values...

--- modules/membrane_blebbing_figures.py
+++ modules/membrane_blebbing_figures.py
@@ -2,19 +2,30 @@
 #
 # D. J. Kelly, 2018-10-15, douglas.kelly@riken.jp
 
-# TODO: refactor as a class so that it is trivial to configure whether images should be
+# TODO?: refactor as a class so that it is trivial to configure whether images should be
 # shown globally, e.g. fig = FigClass(show_figs = False); fig.overlay_curvatures(...)
 # also possibly easier in this case to avoid boilerplate in image saving...
 
 # imports
+import os, sys
+script_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, script_path);
+
 from ij import IJ, ImagePlus, ImageStack
-from ij.gui import Plot
+from ij.gui import Plot, GenericDialog
+from ij.measure import Measurements
 from ij.plugin import RGBStackMerge
-from ij.process import FloatProcessor
+from ij.process import FloatProcessor, ByteProcessor, ColorProcessor
 import membrane_blebbing_engine as mb;
 
-# generate overlays to display curvature
+def pause_for_debug():
+	gd = GenericDialog("Continue?");
+	gd.showDialog();
+	if gd.wasCanceled():
+		raise Exception("Run interupted");
+
 def generate_curvature_overlays(curvature_profile, curvature_stack): 
+	"""Generate overlays to display curvature"""
 	w = curvature_stack.getWidth();
 	ip = FloatProcessor(w, curvature_stack.getHeight());
 	pix = ip.getPixels();
@@ -24,8 +35,8 @@ def generate_curvature_overlays(curvature_profile, curvature_stack):
 	curvature_stack.addSlice(ip);
 	return curvature_stack;
 
-# overlay curvature pixels on membrane image
 def overlay_curvatures(imp, curvature_stack, curvature_profiles, membrane_channel, limits, colormap_string):
+	"""Overlay curvature pixels on membrane image"""
 	overlay_base_imp = imp.clone();
 	overlay_imp = ImagePlus("Curvature stack", curvature_stack);
 	IJ.run(overlay_imp, colormap_string, "");
@@ -44,8 +55,8 @@ def overlay_curvatures(imp, curvature_stack, curvature_profiles, membrane_channe
 		overlaid_stack.addSlice(ip);
 	return ImagePlus("Overlaid curvatures", overlaid_stack), raw_overlay;
 
-# display unnormalised kymograph
 def generate_plain_kymograph(data_to_plot, colormap_string, title_string):
+	"""Display unnormalised kymograph"""
 	ip = FloatProcessor(len(data_to_plot), max([len(data) for data in data_to_plot]));
 	pix = ip.getPixels();
 	for idx, data in enumerate(data_to_plot):
@@ -55,10 +66,9 @@ def generate_plain_kymograph(data_to_plot, colormap_string, title_string):
 	IJ.run(imp, colormap_string, "");
 	imp.show();
 	return imp;
-	
-# display one-channel kymograph with point furthest from the edges along the middle of the
-# kymograph
+
 def generate_kymograph(data_to_plot, colormap_string, title_string):
+	"""Display one-channel kymograph with point furthest from the edges along the middle of the kymograph """
 	kym_height = 2 * max([len(data) for data in data_to_plot]) + 1;
 	ip = FloatProcessor(len(data_to_plot), kym_height);
 	# normalise such that point furthest from the anchors is in the middle of the kymograph
@@ -79,26 +89,54 @@ def generate_kymograph(data_to_plot, colormap_string, title_string):
 	imp.show();
 	return imp;
 
-# generate intensity-weighted curvature image
 def generate_intensity_weighted_curvature(curvature_overlay, curvature_profiles, intensity_channel_imp, colormap_string):
-	overlay_imp = curvature_overlay.clone();
-	base_imp = intensity_channel_imp.clone();
-	IJ.run(overlay_imp, colormap_string, "");
-	IJ.run(overlay_imp, "RGB Color", "");
-	
-	for idx, profile in enumerate(curvature_profiles):
-		overlay_imp.setPosition(idx + 1);
-		base_imp.setPosition(idx + 1);
-		overlay_pix = overlay_imp.getProcessor().getPixels();
-		base_pix = overlay_imp.getProcessor().getPixels();
-		w = overlay_imp.getWidth();
-		for ((x,y), c) in profile:
-			xyidx = int(round(y)) * w + int(round(x));
-			base_pix[xyidx] = base_pix[xyidx] * overlay_pix[xyidx];
-	return base_imp;
+	"""Generate intensity-weighted curvature image"""
+	curv_impRGB = curvature_overlay.clone();
+	IJ.run(curv_impRGB, colormap_string, "");
+	IJ.run(curv_impRGB, "RGB Color", "");
 
-# plot "bleb length"
+	int_imp16 = intensity_channel_imp.clone();
+
+	base_impRGB = intensity_channel_imp.clone();
+	IJ.run(base_impRGB, "Grays", "");
+	IJ.run(base_impRGB, "RGB Color", "");
+	
+	maxes = [];
+	for fridx in range(0, int_imp16.getNFrames()):
+		int_imp16.setPositionWithoutUpdate(1, 1, fridx +1);
+		maxes.append(int_imp16.getStatistics(Measurements.MIN_MAX).max)
+	mx = float(max(maxes));
+
+	print(len(curvature_profiles));
+	pause_for_debug();
+	for idx, profile in enumerate(curvature_profiles):
+		print("Frame = " + str(idx));
+		curv_impRGB.setPosition(idx+1);
+		curvCP = curv_impRGB.getProcessor();
+		base_impRGB.setPosition(idx+1);
+		baseCP = base_impRGB.getProcessor();
+		int_imp16.setPosition(idx+1);
+
+		for chidx in range(0,3):
+			c = ['r', 'g', 'b'];
+			print("Image channel = " + c[chidx]);
+			baseBP = ByteProcessor(base_impRGB.getWidth(), base_impRGB.getHeight());
+			curvBP = ByteProcessor(base_impRGB.getWidth(), base_impRGB.getHeight());
+			baseBP = baseCP.getChannel(chidx, baseBP);
+			curvBP = curvCP.getChannel(chidx, curvBP);
+			
+			for ((x,y), c) in profile:
+				x = int(round(x));
+				y = int(round(y));
+				#baseBP.putPixelValue(x, y, int(curvBP.getPixel(x,y) * float(int_imp16.getPixel(x,y)[0])/mx));
+				baseBP.putPixelValue(x, y, int(curvBP.getPixel(x,y)));
+			baseCP.setChannel(chidx, baseBP);
+		base_impRGB.setProcessor(baseCP);
+	base_impRGB.show();
+	curv_impRGB.show();
+
 def plot_bleb_length(membrane_edges):
+	"""Plot 'bleb length'"""
 	ts = [x for x in range(0, len(membrane_edges))];
 	bleb_ls = [membrane_edge.getLength() for membrane_edge in membrane_edges];
 	plt = Plot("Crude bleb perimeter length against time", "Time, frames", "Bleb length, um");
@@ -107,9 +145,22 @@ def plot_bleb_length(membrane_edges):
 	plot_data = [(t, l) for t, l in zip(ts, bleb_ls)];
 	return plt.getImagePlus(), plot_data
 
-# merge two kymographs
 def merge_kymographs(kym1_imp, kym2_imp):
+	"""Merge two kymographs"""
 	mrg_imp = RGBStackMerge().mergeChannels([kym1_imp, kym2_imp], True);
 	mrg_imp.setTitle("Merged actin intensity and curvature kymograph");
 	mrg_imp.show();
 	return mrg_imp;
+
+#from ij import WindowManager as WM
+#import membrane_blebbing_fileio as mbio;
+#from ij.plugin import ChannelSplitter
+
+#curvature_overlay = IJ.openImage("D:\\data\\Inverse blebbing\\output\\2018-10-17 14-56-47 output\\raw curvature.tif");
+#curvature_profiles = mbio.load_csv_as_profile("D:\\data\\Inverse blebbing\\output\\2018-10-17 14-56-47 output\\curvatures.csv");
+#imp = IJ.openImage("D:\\data\\Inverse blebbing\\MAX_2dpf marcksl1b-EGFP inj_TgLifeact-mCh_movie e4_split-bleb1.tif");
+#intensity_channel_imp = ChannelSplitter.split(imp)[1];
+#intensity_channel_imp.show();
+#colormap_string = "physics"
+
+#generate_intensity_weighted_curvature(curvature_overlay, curvature_profiles, intensity_channel_imp, colormap_string);
